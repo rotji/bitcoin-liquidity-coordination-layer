305d2f4786a5649b49e3419dfeabd79ef001da8d
rotji <starrotji@gmail.com>
2026-02-22 20:52:36 +0100

    Add routing for Protocol and Asset Registry pages
    Integrate ProtocolRegistry and AssetRegistry components
    Enable navigation to new registry feature

--- frontend/bitcoin liquidity network/src/App.tsx
+++ frontend/bitcoin liquidity network/src/App.tsx
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Dashboard from './pages/Dashboard';
+import ProtocolRegistry from './pages/ProtocolRegistry';
+import AssetRegistry from './pages/AssetRegistry';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/protocol-registry" element={<ProtocolRegistry />} />
+        <Route path="/asset-registry" element={<AssetRegistry />} />
       </Routes>
     </>
   );
