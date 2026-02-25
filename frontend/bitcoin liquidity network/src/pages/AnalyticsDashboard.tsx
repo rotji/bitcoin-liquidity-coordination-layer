578ad03df23a8f23687f56e2e566fd6a25a45601
rotji <starrotji@gmail.com>
2026-02-25 23:56:51 +0100

    demo data integration

--- frontend/bitcoin liquidity network/src/pages/AnalyticsDashboard.tsx
+++ frontend/bitcoin liquidity network/src/pages/AnalyticsDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styles from '../styles/AnalyticsDashboard.module.css';
 
 // Temporary in-memory event store for demo
