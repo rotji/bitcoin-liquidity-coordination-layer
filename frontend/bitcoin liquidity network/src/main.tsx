79a9ce5aa5265414fee4b9ec671972ac00bb8555
rotji <starrotji@gmail.com>
2026-02-25 23:55:38 +0100

    resubmited wallet connect

--- frontend/bitcoin liquidity network/src/main.tsx
+++ frontend/bitcoin liquidity network/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import styles from './styles/global.module.css'
 import App from './App.tsx'
+// WalletProvider removed
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
