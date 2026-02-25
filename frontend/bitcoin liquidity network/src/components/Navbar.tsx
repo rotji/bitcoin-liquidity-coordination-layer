b9f9092bb1751e2ec5008774c0e40cd76fb83805
rotji <starrotji@gmail.com>
2026-02-25 23:56:00 +0100

    added wallet connection

--- frontend/bitcoin liquidity network/src/components/Navbar.tsx
+++ frontend/bitcoin liquidity network/src/components/Navbar.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+
+
 import styles from '../styles/Navbar.module.css';
 import { Link } from 'react-router-dom';
+// Wallet integration removed
 
 export default function Navbar() {
+  // Wallet integration removed
   return (
     <nav className={styles.navbar}>
       <div className={styles.navLinks}>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
       </div>
+      {/* Wallet integration UI removed */}
     </nav>
   );
 }
